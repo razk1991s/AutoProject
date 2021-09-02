ccb3b3bbdd3809757d21d6d41be7947614039d00
razk1991s <razk1991s@gmail.com>
2021-09-02 22:49:58 +0300

    Add Files

--- src/main/java/workFlows/APIFlows.java
+++ src/main/java/workFlows/APIFlows.java
@@ -11,4 +11,26 @@ public class APIFlows extends CommonOps {
         response = APIActions.get("/api/teams/search");
         return APIActions.extractFromJSON(response, jPath);
     }
+
+
+
+    @Step("Business Flow: create new team in grafana")
+    public static void postTeam (String name, String email){
+        params.put("name",name);
+        params.put("email",email);
+        APIActions.post(params, "/api/teams");
+    }
+
+    @Step("Business Flow: update existing team grafana")
+    public static void updateTeam (String name, String email, String id){
+        params.put("name",name);
+        params.put("email",email);
+        APIActions.put(params, "/api/teams" + id);
+    }
+
+    @Step("Business Flow: delete existing team in grafana")
+    public static void deleteTeam (String id){
+        APIActions.delete(id);
+    }
+
 }
--- src/test/java/sanity/GrafanaAPI.java
+++ src/test/java/sanity/GrafanaAPI.java
@@ -1,6 +1,7 @@
 package sanity;
 
 import extentions.Verifications;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import utilities.CommonOps;
 import workFlows.APIFlows;
@@ -8,7 +9,31 @@ import workFlows.APIFlows;
 public class GrafanaAPI extends CommonOps {
 
     @Test(description = "test 01 get team from grafana")
+    @Description("this test verify team property")
     public void test_01_verify_team(){
-        Verifications.verifyText(APIFlows.getTeamProperty("teams[0].name"),"yoni");
+        Verifications.verifyText(APIFlows.getTeamProperty("teams[0].name"),"raz");
+    }
+
+    @Test(description = "test 02 add team and verify")
+    @Description("this test add team to grafana and verify it")
+    public void test_02_add_team_and_verify(){
+        APIFlows.postTeam("razteam","raz@team.com");
+        Verifications.verifyText(APIFlows.getTeamProperty("teams[1].name"),"razteam");
+    }
+
+    @Test(description = "test 03 update team and verify")
+    @Description("this test update team in grafana and verify it")
+    public void test_03_update_team_and_verify(){
+        String id = APIFlows.getTeamProperty("teams[1].id");
+        APIFlows.updateTeam("razteam","raz@somthing.com",id);
+        Verifications.verifyText(APIFlows.getTeamProperty("teams[1].email"),"raz@somthing.com");
+    }
+
+    @Test(description = "test 04 delete team and verify")
+    @Description("this test delete team to grafana and verify it")
+    public void test_04_delete_team_and_verify(){
+        String id = APIFlows.getTeamProperty("teams[1].id");
+        APIFlows.deleteTeam(id);
+        Verifications.verifyText(APIFlows.getTeamProperty("totalCount"),"1");
     }
 }
